ba91e389e806a2701dbc7c400c2117d0b737a8a7
Amin Rozas <aminrozas24@gmail.com>
2025-12-03 18:02:40 +0100

    création formulaire

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import {Component, NgModule} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from './components/header/header';
 import {Footer} from './components/footer/footer';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
 export class Contact {
 
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      // 👉 Ici tu enverras via HTTP vers ton backend ou email service
+    }
+  }
 }
