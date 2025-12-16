90ec613bd13c5110d15d7b07a937c90d41500377
Aminisco <aminrozas24@gmail.com>
2025-12-16 20:45:50 +0100

    page asbl + texte projets

--- src/app/pages/page-asbl/page-asbl.ts
+++ src/app/pages/page-asbl/page-asbl.ts
@@ -10,52 +10,53 @@ import {RouterLink} from '@angular/router';
   ],
   templateUrl: './page-asbl.html',
   styleUrl: './page-asbl.scss',
+  standalone: true
 })
 export class PageAsbl {
 
   team = [
     {
-      name: 'Rozas Zabalo Amin',
+      name: 'Amin Rozas Zabalo',
       role: 'Trésorier',
       photo: 'assets/image-asbl/membres/Amin.jpg',
-      bio: 'Developpeur informatique',
-      email: 'email@example.com',
+      bio: "Issu d'une formation en informatique, je pense qu'il est important de venir en aide à la jeunesse via différents projets",
+      email: 'aminrozas24@gmail.com',
       linkedin: '',
       facebook: ''
     },
     {
-      name: 'Alarcon Isco',
-      role: 'Coordinateur',
-      photo: 'assets/image-asbl/membres/Avatar2.jpg',
-      bio: "Pas d'inspiration donc j'écris au hasard sa mère",
-      email: 'email@example.com',
+      name: 'Anas Bentatou',
+      role: 'Secrétaire',
+      photo: 'assets/image-asbl/membres/anas.jpg',
+      bio: "Animateur et éducateur en accompagnement psycho-éducatif avec une grande expérience dans l'Aide à la Jeunesse et la vie institutionnelle",
+      email: 'benta4525@gmail.com',
       linkedin: '',
       facebook: ''
     },
     {
-      name: 'Dubois Julien',
+      name: 'Julien Dubois',
       role: 'Président',
-      photo: 'assets/image-asbl/membres/julien.jpg',
-      bio: "Le big J, le boss de l'asso, tranquille c'est une imaghe d'exemple",
-      email: 'email@example.com',
+      photo: 'assets/image-asbl/membres/julien2.jpg',
+      bio: "Fondateur de projets associatifs et éducateur spécialisé en accompagnement psycho-éducatif avec une grande expérience dans l'Aide à la Jeunesse et la vie institutionnelle",
+      email: 'julienissa.dubois@gmail.com',
       linkedin: '',
       facebook: ''
     },
     {
-      name: 'Leo Messi',
-      role: 'Goat',
-      photo: 'assets/image-asbl/membres/MESSI.jpg',
-      bio: "Le goat tout simplement",
-      email: 'email@example.com',
+      name: 'Adam Ghannan',
+      role: 'Animateur',
+      photo: 'assets/image-asbl/membres/adam.jpg',
+      bio: "Animateur à Mosaïc ASBL avec une formation de régisseur dans l'événementiel",
+      email: 'ghannanadam@outlook.be',
       linkedin: '',
       facebook: ''
     },
     {
-      name: 'Rozas Zabalo Amin',
-      role: 'Trésorier',
-      photo: 'assets/image-asbl/membres/Amin.jpg',
-      bio: 'Developpeur informatique',
-      email: 'email@example.com',
+      name: 'Saphae Allaoui',
+      role: 'Graphic designer',
+      photo: 'assets/image-asbl/membres/saphae.jpg',
+      bio: 'Infirmière en soins généraux de formation',
+      email: 'saphae.allaoui@outlook.fr',
       linkedin: '',
       facebook: ''
     }
